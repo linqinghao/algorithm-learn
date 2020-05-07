85b331b77c54bd94213f26e5c653cc62b41eda2e
heiyelin <heiyezoz@gmail.com>
2020-05-08 00:14:56 +0800

    refactor: update

--- algorithm/sort/quick-sort/quick-sort-2.js
+++ algorithm/sort/quick-sort/quick-sort-2.js
@@ -2,25 +2,27 @@
  * 快速排序(in-place)
  */
 
-function quickSort(arr, left = 0, right = arr.length - 1) {
-  if (left < right) {
-    const pivot = partition(arr, left, right)
-    quickSort(arr, left, pivot - 1)
-    quickSort(arr, pivot + 1, right)
+function quickSort(arr, start = 0, end = arr.length - 1) {
+  if (end - start < 1) {
+    return
   }
-  return arr
-}
-
-function partition(arr, left, right) {
-  let pivot = left
-  for (let i = left + 1; i <= right; i++) {
-    if (arr[i] < arr[left]) {
-      ;[arr[i], arr[pivot]] = [arr[pivot], arr[i]]
-      pivot += 1
+  const target = arr[start]
+  let l = start
+  let r = end
+  while (l < r) {
+    while (l < r && arr[r] >= target) {
+      r--
     }
+    arr[l] = arr[r]
+    while (l < r && arr[l] < target) {
+      l++
+    }
+    arr[r] = arr[l]
   }
-  ;[arr[left], arr[pivot]] = [arr[pivot], arr[left]]
-  return pivot
+  arr[l] = target
+  quickSort(arr, start, l - 1)
+  quickSort(arr, l + 1, end)
+  return arr
 }
 
 console.log(quickSort([5, 6, 4, 8, 3, 2, 9, 1, 7]))
